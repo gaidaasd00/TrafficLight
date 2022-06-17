ca1f71074428e8d53d2c527c20a9aed53ed3fe8c
gaidaasd00 <swiftgaida@list.ru>
2022-06-17 21:29:42 +0300

    1. Added components 2. Added code logic

---
+++ TrafficLight/CurrentLight.swift
@@ -0,0 +1,12 @@
+//
+//  CurrentLight.swift
+//  TrafficLight
+//
+//  Created by Алексей Гайдуков on 17.06.2022.
+//
+
+import Foundation
+
+enum CurrentLight {
+    case red, yellow, green
+}
--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLable: UIView!
+    @IBOutlet var yellowLable: UIView!
+    @IBOutlet var greenLable: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.1
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLable.layer.cornerRadius = 50
+        yellowLable.layer.cornerRadius = 50
+        greenLable.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 10
+        
+        redLable.alpha = lightOff
+        yellowLable.alpha = lightOff
+        greenLable.alpha = lightOff
+        
+        
+        
     }
 
-
+    @IBAction func startButtonTapped() {
+        startButton.setTitle(redLable.isHidden ? "START" : "NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLable.alpha = lightOn
+            greenLable.alpha = lightOff
+            currentLight = .yellow
+        case .yellow:
+            redLable.alpha = lightOff
+            yellowLable.alpha = lightOn
+            currentLight = .green
+        case .green:
+            yellowLable.alpha = lightOff
+            greenLable.alpha = lightOn
+            currentLight = .red
+        }
+        
+    }
+    
 }
 
